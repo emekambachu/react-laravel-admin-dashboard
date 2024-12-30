77b28454d902c85e2b2c57a4381d486466580ae1
Victor Emeka Mbachu <victor.c.mbachu@gmail.com>
2024-12-30 23:50:55 +0000

    working on router

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -2,6 +2,7 @@ import {createBrowserRouter} from "react-router-dom";
 import {Login} from "./pages/Login.jsx";
 import {Signup} from "./pages/Signup.jsx";
 import {Users} from "./pages/Users.jsx";
+import {NotFound} from "./pages/NotFound.jsx";
 
 const router = createBrowserRouter([
 
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
   {
     path: '/login',
     element: <Users />
+  },
+  {
+    path: '*',
+    element: <NotFound />
   }
 
 ]);
