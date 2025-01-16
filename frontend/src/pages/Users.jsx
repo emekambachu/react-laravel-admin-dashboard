55cd465e894de515d953673652d803c86c1b21d4
Victor Emeka Mbachu <victor.c.mbachu@gmail.com>
2025-01-16 16:56:48 +0000

    working on user controller

--- frontend/src/pages/Users.jsx
+++ frontend/src/pages/Users.jsx
@@ -1,4 +1,31 @@
+import {useEffect, useState} from "react";
+import axiosClient from "../utils/axios-client.js";
+
 export const Users = () => {
+
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+
+    getUsers();
+
+  }, []);
+
+  const getUsers = () => {
+
+    setLoading(true);
+
+    axiosClient.get('/users')
+      .then(({data}) => {
+        setUsers(data);
+      }).catch(err => {
+      console.log(err);
+    });
+
+    setLoading(false);
+  }
+
     return (
         <>
           <div>
